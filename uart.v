c2ab786c6c640d61a84c212faeec8d922c7dbb90
Patcybermind <pato.gonzalezcabrera@gmail.com>
2024-09-05 20:06:17 -0400

    uart tx works

--- uart.v
+++ uart.v
@@ -1,6 +1,6 @@
 `default_nettype none
 
-module uart
+module top
 #(
     parameter DELAY_FRAMES = 234 // 27,000,000 (27Mhz) / 115200 Baud rate
 )
@@ -14,6 +14,7 @@ module uart
 
     localparam HALF_DELAY_WAIT = (DELAY_FRAMES / 2);
 
+// rx
 reg [3:0] rxState = 0;
 reg [12:0] rxCounter = 0;
 reg [2:0] rxBitNumber = 0;
@@ -26,6 +27,99 @@ localparam RX_STATE_READ_WAIT = 2;
 localparam RX_STATE_READ = 3;
 localparam RX_STATE_STOP_BIT = 5;
 
+// tx
+reg [3:0] txState = 0;
+reg [24:0] txCounter = 0;
+reg [7:0] dataOut = 0;
+reg txPinRegister = 1;
+reg [2:0] txBitNumber = 0;
+reg [3:0] txByteCounter = 0;
+
+assign uart_tx = txPinRegister;
+
+localparam MEMORY_LENGTH = 12;
+reg [7:0] testMemory [MEMORY_LENGTH-1:0];
+
+initial begin
+    testMemory[0] = "h";
+    testMemory[1] = "e";
+    testMemory[2] = "l";
+    testMemory[3] = "l";
+    testMemory[4] = "o";
+    testMemory[5] = " ";
+    testMemory[6] = "w";
+    testMemory[7] = "o";
+    testMemory[8] = "r";
+    testMemory[9] = "l";
+    testMemory[10] = "d";
+    testMemory[11] = " ";
+end
+
+localparam TX_STATE_IDLE = 0;
+localparam TX_STATE_START_BIT = 1;
+localparam TX_STATE_WRITE = 2;
+localparam TX_STATE_STOP_BIT = 3;
+localparam TX_STATE_DEBOUNCE = 4;
+
+always @(posedge clk) begin
+    case (txState)
+        TX_STATE_IDLE: begin
+            if (btn1 == 0) begin
+                txState <= TX_STATE_START_BIT;
+                txCounter <= 0;
+                txByteCounter <= 0;
+            end
+            else begin
+                txPinRegister <= 1;
+            end
+        end 
+        TX_STATE_START_BIT: begin
+            txPinRegister <= 0;
+            if ((txCounter + 1) == DELAY_FRAMES) begin
+                txState <= TX_STATE_WRITE;
+                dataOut <= testMemory[txByteCounter];
+                txBitNumber <= 0;
+                txCounter <= 0;
+            end else 
+                txCounter <= txCounter + 1;
+        end
+        TX_STATE_WRITE: begin
+            txPinRegister <= dataOut[txBitNumber];
+            if ((txCounter + 1) == DELAY_FRAMES) begin
+                if (txBitNumber == 3'b111) begin
+                    txState <= TX_STATE_STOP_BIT;
+                end else begin
+                    txState <= TX_STATE_WRITE;
+                    txBitNumber <= txBitNumber + 1;
+                end
+                txCounter <= 0;
+            end else 
+                txCounter <= txCounter + 1;
+        end
+        TX_STATE_STOP_BIT: begin
+            txPinRegister <= 1;
+            if ((txCounter + 1) == DELAY_FRAMES) begin
+                if (txByteCounter == MEMORY_LENGTH - 1) begin
+                    txState <= TX_STATE_DEBOUNCE;
+                end else begin
+                    txByteCounter <= txByteCounter + 1;
+                    txState <= TX_STATE_START_BIT;
+                end
+                txCounter <= 0;
+            end else 
+                txCounter <= txCounter + 1;
+        end
+        TX_STATE_DEBOUNCE: begin
+            if (txCounter == 23'b111111111111111111) begin
+                if (btn1 == 1) 
+                    txState <= TX_STATE_IDLE;
+            end else
+                txCounter <= txCounter + 1;
+        end
+    endcase
+end
+
+// rx
 always @(posedge clk) begin
     if (byteReady) begin
         led <= ~dataIn[5:0];
